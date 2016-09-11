8800191cb6d71b69edf9de98391435e330512e16
MyrulXzavier (GamerXzavier) <GamerXzavier@gmail.com>
2016-09-11 20:32:44 +0800

    Update Core.php

--- src/Cardinals/Core.php
+++ src/Cardinals/Core.php
@@ -5,9 +5,16 @@ namespace Cardinals;
 class Core{
 
  function Core(){
+   self::init();
+   self::instance();
    
 }
 
+ $motion = 1;//by default
+ public function motion($motion){
+  $this->Core()->motion($motion);
+
+}
 
 
 }
